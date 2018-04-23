49407855e62838d5fc39d9f8d649e623f4482397
DESKTOP-6T48EI7\DJX <engchris95@gmail.com>
2018-04-24 00:55:31 +0100

    Made changes to landing page added contact page

---
+++ contact.php
@@ -0,0 +1,53 @@
+<?php include 'header.html'; ?>
+    <div class="wrapper">
+      <div class="header header-filter1" style="background-image: url('assets/img/background/contactus.png'); background-size: contain; background-repeat: no-repeat;">
+        <div class="overlay">
+          <div class="container">
+            <div class="row">
+  						<div class="col-md-6">
+  							<h1 class="title">Contact Us</h1>
+  	            <h4>Are you an artisan willing to work with us. Clients are ready to meet up with you as soon as possible, anywhere, 24/7.</h4>
+  						</div>
+            </div>
+          </div>
+        </div>
+      </div>
+			<div class="main main-raised">
+				<div class="container">
+	      	<div class="section landing-section">
+	          <div class="row">
+	            <div class="col-md-8 col-md-offset-2">
+	              <h2 class="text-center title">Work with us</h2>
+	              <form class="contact-form">
+	                <div class="row">
+	                  <div class="col-md-6">
+											<div class="form-group label-floating">
+												<label class="control-label">Your Name</label>
+												<input type="email" class="form-control">
+											</div>
+	                  </div>
+	                  <div class="col-md-6">
+											<div class="form-group label-floating">
+												<label class="control-label">Your Email</label>
+												<input type="email" class="form-control">
+											</div>
+	                  </div>
+	                </div>
+									<div class="form-group label-floating">
+										<label class="control-label">Your Messge</label>
+										<textarea class="form-control" rows="4"></textarea>
+									</div>
+	                <div class="row">
+	                    <div class="col-md-4 col-md-offset-4 text-center">
+	                        <button class="btn btn-primary btn-raised">
+														Send Message
+													</button>
+	                    </div>
+	                </div>
+	              </form>
+	            </div>
+	          </div>
+	        </div>
+	      </div>
+			</div>
+      <?php include 'resources/footer.php'; ?>
--- index.php
+++ index.php
@@ -1,56 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="utf-8" />
-	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-	<!-- <link rel="icon" type="image/png" href="assets/img/favicon.png"> -->
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
-	<title>Artisan</title>
-
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-
-	<!--     Fonts and icons     -->
-	<link rel="stylesheet" href="assets/css/font-awesome.min.css" />
-
-	<!-- CSS Files -->
-  <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="assets/css/material-kit.css" rel="stylesheet"/>
-	<link href="assets/css/main.css" rel="stylesheet"/>
-
-</head>
-
-<body class="landing-page">
-    <nav class="navbar navbar-absolute navbar-fixed-top">
-    	<div class="container">
-        	<!-- Brand and toggle get grouped for better mobile display -->
-        	<div class="navbar-header">
-        		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example">
-            		<span class="sr-only">Toggle navigation</span>
-		            <span class="icon-bar"></span>
-		            <span class="icon-bar"></span>
-		            <span class="icon-bar"></span>
-        		</button>
-        		<a class="navbar-brand" href="index.html">aRTISIAN</a>
-        	</div>
-
-        	<div class="collapse navbar-collapse" id="navigation-example">
-        		<ul class="nav navbar-nav navbar-right">
-							<li>
-								<a href="#">Home</a>
-							</li>
-							<li>
-								<a href="#">Register</a>
-							</li>
-							<li>
-								<a href="#">Contact Us</a>
-							</li>
-        		</ul>
-        	</div>
-    	</div>
-    </nav>
+<?php include 'header.html'; ?>
     <div class="wrapper">
-      <div class="header header-filter" style="background-image: url('assets/img/background/advert.jpeg');">
+      <div class="header header-filter" style="background-image: url('assets/img/background/advert.jpeg')">
         <div class="container">
           <div class="row">
 						<div class="col-md-6">
@@ -82,18 +32,58 @@
 							<button type="button" name="button" class="btn btn-primary findOut">Find out</button>
 						</div>
 						<div class="col-md-6">
-							<h3 class="title">Hiring Artisans just gort easier</h3>
-							<p>
-								We go the extra mile to ensure your satisfaction. When you hire a handyman, we single out the best candidate for the job who has gone through our background check and rigorous levels of scrutiny.
-							</p>
-							<button type="button" name="button" class="btn btn-primary findOut">Find out</button>
+							<h3 class="title">Frequently Asked Question</h3>
+							<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+							  <div class="panel panel-default">
+							    <div class="panel-heading" role="tab" id="headingOne">
+							      <h4 class="panel-title">
+							        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+							          Who are we?
+							        </a>
+							      </h4>
+							    </div>
+							    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+							      <div class="panel-body">
+											We act as middle men between artisans and clients who needs their services. We provide our clients with highly skilled, efficient and reliable artisans.
+										</div>
+							    </div>
+							  </div>
+							  <div class="panel panel-default">
+							    <div class="panel-heading" role="tab" id="headingTwo">
+							      <h4 class="panel-title">
+							        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+												Who takes responsibility for any damages caused by artisans?
+							        </a>
+							      </h4>
+							    </div>
+							    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+							      <div class="panel-body">
+							        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+							      </div>
+							    </div>
+							  </div>
+							  <div class="panel panel-default">
+							    <div class="panel-heading" role="tab" id="headingThree">
+							      <h4 class="panel-title">
+							        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+							          How do i make payment?
+							        </a>
+							      </h4>
+							    </div>
+							    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+							      <div class="panel-body">
+							        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+							      </div>
+							    </div>
+							  </div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</section>
 			<section id="easierArtisans">
 				<div class="container">
-					<h3 class="title text-center" style="margin-bottom: 40px;">Hiring Artisans just got easier</h3>
+					<h3 class="title text-center" style="margin-bottom: 40px;">Hiring Artisans just got easier!</h3>
 					<div class="row">
 						<div class="col-md-4">
 							<img src="assets/img/icons/describe.png" alt="" class="img img-responsive center-block" width="75px" />
